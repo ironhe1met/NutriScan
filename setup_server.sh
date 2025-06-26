15c467407b571357a5fcd2103cd7a4f0c4e37751
Eugene <64968836+ironhe1met@users.noreply.github.com>
2025-06-26 16:16:46 +0300

    Pin dependency versions

--- setup_server.sh
+++ setup_server.sh
@@ -4,6 +4,6 @@ set -e
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt  # встановлюємо залежності фіксованих версій
 python scripts/download_weights.py
 python run.py
