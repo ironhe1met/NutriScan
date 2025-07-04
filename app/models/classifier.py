e0453fbebbeed6ac13b8ba0467152835ee1d46df
ironhe1met <e.v.chernenko.7@gmail.com>
2025-07-04 18:58:41 +0300

    оновлено

--- app/models/classifier.py
+++ app/models/classifier.py
@@ -9,8 +9,15 @@ LABELS_PATH = "app/assets/labels_food101.json"
 
 class FoodClassifier:
     def __init__(self):
-        self.model = torch.load(MODEL_PATH, map_location="cpu")
+#        self.model = torch.load(MODEL_PATH, map_location="cpu")
+#        self.model.eval()
+        from efficientnet_pytorch import EfficientNet
+
+        self.model = EfficientNet.from_name('efficientnet-b0')
+        state_dict = torch.load(MODEL_PATH, map_location="cpu")
+        self.model.load_state_dict(state_dict)
         self.model.eval()
+
 
         with open(LABELS_PATH, "r") as f:
             self.labels = json.load(f)
@@ -31,7 +38,12 @@ class FoodClassifier:
             probs = torch.nn.functional.softmax(outputs[0], dim=0)
             confidence, predicted_idx = torch.max(probs, 0)
 
+#        return {
+#            "dish_name": self.labels[str(predicted_idx.item())],
+#            "confidence": round(confidence.item(), 4)
+#        }
+        id2label = [label for label, idx in sorted(self.labels.items(), key=lambda x: x[1])]
         return {
-            "dish_name": self.labels[str(predicted_idx.item())],
+            "dish_name": id2label[predicted_idx.item()],
             "confidence": round(confidence.item(), 4)
         }
