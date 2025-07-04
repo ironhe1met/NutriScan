c730271968ee827954f9c635ab38b5a32975e9d8
ironhe1met <e.v.chernenko.7@gmail.com>
2025-07-04 16:05:11 +0300

    додав автоматизації до скрипта scripts/download_models.sh

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -e
+
 mkdir -p models
-mkdir -p app/assets
 
-echo "⬇️ Завантаження classifier.pt..."
-wget -O models/classifier.pt https://huggingface.co/dwililiya/food101-model-classification/resolve/main/pytorch_model.bin
-
-echo "⬇️ Завантаження labels_food101.json..."
-wget -O app/assets/labels_food101.json https://huggingface.co/dwililiya/food101-model-classification/resolve/main/label2id.json
+echo "⬇️ Завантаження моделі EfficientNet-B0 (Food-101)..."
+wget -O models/classifier.pth \
+  https://huggingface.co/dwililiya/food101-model-classification/resolve/main/best_model.pth
 
 echo "✅ Завантаження завершено"
