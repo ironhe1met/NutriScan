95afd03d3cc3ffb6a8efd7204203a53c3c39cc96
ironhe1met <e.v.chernenko.7@gmail.com>
2025-07-09 14:43:17 +0300

    update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,19 +1,22 @@
+# app/api/routes.py
+
 from fastapi import APIRouter, UploadFile, File
-from fastapi.responses import JSONResponse
 from app.models.detector import IngredientDetector
-import shutil
+from app.services.nutrition import NutritionService
 import tempfile
 
 router = APIRouter()
 
-detector = IngredientDetector("models/yolov8x-seg.pt")
+detector = IngredientDetector()
+nutrition = NutritionService()
 
 @router.post("/detect/")
 async def detect_ingredients(image: UploadFile = File(...)):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
-        shutil.copyfileobj(image.file, tmp)
+        tmp.write(await image.read())
         tmp_path = tmp.name
 
-    result = detector.detect(tmp_path)
-#    return JSONResponse(content={"ingredients": result})
-    return JSONResponse(content=result)
+    raw = detector.detect(tmp_path)
+    enriched = nutrition.enrich_ingredients(raw["ingredients"])
+
+    return enriched
