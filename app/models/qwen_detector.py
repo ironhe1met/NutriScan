1aa69f0bba87cd8a316905a49b122db4cd32994c
ironhe1met <e.v.chernenko.7@gmail.com>
2025-07-22 15:16:56 +0300

    update file

--- app/models/qwen_detector.py
+++ app/models/qwen_detector.py
@@ -2,12 +2,13 @@ import io, json
 from PIL import Image
 import torch
 from transformers import AutoProcessor, QwenForConditionalGeneration
+from transformers import AutoProcessor, Qwen2_5_VLForConditionalGeneration
 
 class QwenFoodDetector:
     def __init__(self, model_dir: str):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.processor = AutoProcessor.from_pretrained(model_dir, trust_remote_code=False)
-        self.model = QwenForConditionalGeneration.from_pretrained(model_dir, trust_remote_code=False).to(self.device)
+        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_dir, trust_remote_code=False).to(self.device)
         self.model.eval()
 
     def detect(self, image_path: str) -> dict:
