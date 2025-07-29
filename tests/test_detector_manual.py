0bc5502b3bcf29d9391323de47064493d48bcfaa
ironhe1met <e.v.chernenko.7@gmail.com>
2025-07-29 12:43:29 +0300

    update folder

--- tests/test_detector_manual.py
+++
@@ -1,7 +0,0 @@
-from app.models.detector import IngredientDetector
-
-if __name__ == "__main__":
-    detector = IngredientDetector("models/yolov8x-seg.pt")
-    image_path = "tests/test_image.jpg"
-    result = detector.detect(image_path)
-    print(result)
